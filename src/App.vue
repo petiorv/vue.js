0f780cb1211b03ec3233225b9925df3434c70c9b
pvasilev <petyo.vasilev@superdev.com>
2018-12-17 17:34:06 +0200

    initial

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <header>
+      <nav>
+        <ul>
+          <li class="nav-item">
+            Home
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main>    
+    <HomePage/>
+    </main>
+  </div>
+</template>
+
+<script>
+import HomePage from './components/HomePage.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HomePage
+  }
+}
+</script>
+
+<style>
+body{
+  background: linear-gradient(to bottom, #556, #999);
+  background-attachment: fixed;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  
+}
+main{
+  margin: 0 auto;
+  padding: 30px;
+  background-color: white;
+  width: 1024px;
+  min-height: 300px;
+}
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+      <div class="robot">
+        <img class="robot" src="../assets/robot.png">
+      </div>
+      <h1>Hello</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home{
+  text-align: center;
+}
+.robot{  
+  height: 300px;
+}
+</style>
