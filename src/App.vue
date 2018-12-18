22fc1a6ed41925a0b2faf83e48362016ba39ece2
pvasilev <petyo.vasilev@superdev.com>
2018-12-18 15:20:57 +0200

    add some images

--- src/App.vue
+++ src/App.vue
@@ -16,8 +16,8 @@
 </template>
 
 <script>
-import HomePage from './components/HomePage.vue'
-
+import HomePage from './Home/HomePage.vue'
+import RobotBuilder from './build/RobotBuilder.vue';
 export default {
   name: 'app',
   components: {
@@ -41,5 +41,24 @@ main{
   background-color: white;
   width: 1024px;
   min-height: 300px;
+}
+header {
+  background-color: #999;
+  width: 1084px;
+  margin: 0 auto;
+}
+ul {
+  padding: 3px;
+  display: flex;
+}
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+.logo {
+  vertical-align: middle;
+  height: 30px;
 }
 </style>
---
+++ src/build/RobotBuilder.vue
@@ -0,0 +1,132 @@
+<template>   
+<div>
+    <div class="top-row">
+      <div class="top part">
+        <img src="./images/head-big-eye.png" title="head"/>
+        <button class="prev-selector">&#9668;</button>
+        <button class="next-selector">&#9658;</button>
+      </div>
+    </div>
+    <div class="middle-row">
+      <div class="left part">
+        <img src="./images/arm-articulated-claw.png" title="left arm"/>
+        <button class="prev-selector">&#9650;</button>
+        <button class="next-selector">&#9660;</button>
+      </div>
+      <div class="center part">
+        <img src="./images/torso-flexible-gauged.png" title="left arm"/>
+        <button class="prev-selector">&#9668;</button>
+        <button class="next-selector">&#9658;</button>
+      </div>
+      <div class="right part">
+        <img src="./images/arm-dual-claw.png" title="left arm"/>
+        <button class="prev-selector">&#9650;</button>
+        <button class="next-selector">&#9660;</button>
+      </div>
+    </div>
+    <div class="bottom-row">
+      <div class="bottom part">
+        <img src="./images/base-single-wheel.png" title="left arm"/>
+        <button class="prev-selector">&#9668;</button>
+        <button class="next-selector">&#9658;</button>
+      </div>
+    </div>
+  </div> 
+</template>
+
+<script>
+export default{
+    name: 'RobotBuilder'
+};
+</script>
+
+<style>
+.part {
+  position: relative;
+  width:165px;
+  height:165px;
+  border: 3px solid #aaa;
+} 
+.part img {
+  width:165px;
+}
+.top-row {
+  display:flex;
+  justify-content: space-around;
+}
+.middle-row {
+  display:flex;
+  justify-content: center;
+}
+.bottom-row {
+  display:flex;
+  justify-content: space-around;
+  border-top: none;
+}
+.head {
+  border-bottom: none;
+}
+.left {
+  border-right: none;
+}
+.right {
+  border-left: none;
+}
+.left img {
+  transform: rotate(-90deg);
+}
+.right img {
+  transform: rotate(90deg);
+}
+.bottom {
+  border-top: none;
+}
+.prev-selector {
+  position: absolute;
+  z-index:1;
+  top: -3px;
+  left: -28px;
+  width: 25px;
+  height: 171px;
+}
+.next-selector {
+  position: absolute;
+  z-index:1;
+  top: -3px;
+  right: -28px;
+  width: 25px;
+  height: 171px;
+}
+.center .prev-selector, .center .next-selector {
+  opacity:0.8;
+}
+.left .prev-selector {
+  top: -28px;
+  left: -3px;
+  width: 144px;
+  height: 25px;
+}
+.left .next-selector {
+  top: auto;
+  bottom: -28px;
+  left: -3px;    
+  width: 144px;
+  height: 25px;
+}
+.right .prev-selector {
+  top: -28px;
+  left: 24px;  
+  width: 144px;
+  height: 25px;
+}
+.right .next-selector {
+  top: auto;
+  bottom: -28px;
+  left: 24px;    
+  width: 144px;
+  height: 25px;
+}
+.right .next-selector {
+  right: -3px;
+}
+</style>
